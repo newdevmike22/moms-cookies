757d67e01a081f8f1050290d95a90646bdf92965
MLDODSON <paola@dominicansoul.com>
2024-01-23 11:49:07 -0600

    Branch 5
    Branch 5 - Filtering the menu item's categories.

--- cookies/src/App.jsx
+++ cookies/src/App.jsx
@@ -16,12 +16,21 @@ const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+
+    const newItems = menu.filter((item) => item.category === category );
+    setMenuItems(newItems);
+  };
 
   return (
     <main className="w-[90vw] max-w-[1120px] my-[5rem] mx-[auto]">
       <section className="py-[5rem] px-0">
         <Title text="mom's delights" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- cookies/src/components/Categories.jsx
+++ cookies/src/components/Categories.jsx
@@ -1,4 +1,4 @@
-const Categories = ({ categories}) => {
+const Categories = ({ categories, filterItems }) => {
     return (
         <div className="mt-8 mr-0 mb-16 ml-0 flex wrap justify-center gap-4">
             {categories.map((category) => {
@@ -7,6 +7,7 @@ const Categories = ({ categories}) => {
                         className="block cursor-pointer text-white uppercase font-semibold bg-[#f70707] hover:bg-[#ff03c0] hover:shadow-md hover:animate-bounce 
                                    border-transparent rounded-md tracking-wide py-[0.375rem] px-3 shadow-md transition-all duration-300 ease-in-out"
                         key={category}
+                        onClick={() => filterItems(category)}
                     >
                         {category}
                     </button>
